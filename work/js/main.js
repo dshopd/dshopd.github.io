c0fe5d130597bcc7f2521019462d2ead6152a851
dshopd <ushanskiu@yandex.ru>
2017-10-04 17:37:29 +0300

    'news-work'

---
+++ work/js/main.js
@@ -0,0 +1,15 @@
+
+$('.slick-animation').slick({
+  infinite: false,
+  dots: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  speed: 100,
+  fade: true,
+  cssEase: 'linear',
+  initialSlide: 1,
+  prevArrow: $('.left'),
+  nextArrow: $('.right'),
+});
+
